b87058fec26ff83945024755d869efce5eae225b
yehia199 <yehiacomputer08@gmail.com>
2024-08-30 19:19:01 +0300

    updated hyprland config

--- hypr/scripts/sleep.sh
+++ hypr/scripts/sleep.sh
@@ -1,5 +1,1 @@
- swayidle -w timeout 600 'swaylock -f -c 000000' \
-             timeout 600 'hyprctl dispatch dpms off' \
-             resume 'hyprctl dispatch dpms on' \
-             timeout 1200 'systemctl suspend' \
-             before-sleep 'swaylock -f -c 000000' &
+ 
